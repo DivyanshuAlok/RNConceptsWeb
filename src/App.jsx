2624a04be17291167d79f4c994933277ac20c623
DivyanshuAlok <divyanshuncs@gmail.com>
2024-03-17 13:33:24 +0530

    Added redux stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
+<<<<<<< Updated upstream
 import Home from "./LiciousBookStore/Home";
+=======
+import ReduxHome from "./Redux/ReduxHome";
+>>>>>>> Stashed changes
 
 const App = () => {
-  return <Home />;
+  return <ReduxHome />;
 };
 
 export default App;
---
+++ src/Redux/ReduxHome.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, Appearance } from "react-native";
+import React, { useEffect } from "react";
+
+const ReduxHome = () => {
+  useEffect(() => {
+    //Appearance.setColorScheme("dark");
+  }, []);
+  return (
+    <View>
+      <Text>ReduxHome</Text>
+    </View>
+  );
+};
+
+export default ReduxHome;
+
+const styles = StyleSheet.create({});
